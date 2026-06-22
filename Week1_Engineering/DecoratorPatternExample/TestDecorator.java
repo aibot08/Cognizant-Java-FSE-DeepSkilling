a470ed66d35cdf3cfe664baa6da104a35e3e9165
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-22 17:00:58 +0530

    Implement TestDecorator.java

---
+++ Week1_Engineering/DecoratorPatternExample/TestDecorator.java
@@ -0,0 +1,16 @@
+package com.cognizant.decorator;
+
+public class TestDecorator {
+
+    public static void main(String[] args) {
+
+        Notifier notifier =
+                new SlackNotifierDecorator(
+                        new SMSNotifierDecorator(
+                                new EmailNotifier()
+                        )
+                );
+
+        notifier.send();
+    }
+}
