f1d15a81c40573d3ca5bd189b5804cdfba0724bf
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-22 23:41:52 +0530

    Add TestProxy.java

---
+++ Week1_Engineering/ProxyPatternExample/TestProxy.java
@@ -0,0 +1,15 @@
+package com.cognizant.proxy;
+
+public class TestProxy {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("photo.jpg");
+
+        image.display();
+
+        System.out.println();
+
+        image.display();
+    }
+}
