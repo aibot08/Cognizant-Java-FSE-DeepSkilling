32b637d63d4219fdde84267d72c732bbf0615050
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-22 15:16:05 +0530

    Add PayPalGateway.java

---
+++ Week1_Engineering/Week1_Engineering/AdapterPatternExample/PayPalGateway.java
@@ -0,0 +1,8 @@
+package com.cognizant.adapter;
+
+public class PayPalGateway {
+
+    public void makePayment(double amount) {
+        System.out.println("Payment of Rs." + amount + " processed through PayPal");
+    }
+}
