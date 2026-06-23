478b4391585ad8fd778be9e66e00e24911cab10c
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-23 15:54:02 +0530

    Add MobileApp.java

---
+++ Week1_Engineering/ObserverPatternExample/MobileApp.java
@@ -0,0 +1,17 @@
+package com.cognizant.observer;
+
+public class MobileApp implements Observer {
+
+    private String user;
+
+    public MobileApp(String user) {
+        this.user = user;
+    }
+    public void update(String stockName, double price) {
+
+        System.out.println(
+                "Mobile App - " + user +
+                " notified: " + stockName +
+                " price is Rs." + price);
+    }
+}
