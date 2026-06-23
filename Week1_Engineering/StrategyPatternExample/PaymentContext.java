59e1f2b8dea47afcd42664d2052a1ec48f9ec68e
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-23 16:02:57 +0530

    Add PaymentContext.java

---
+++ Week1_Engineering/StrategyPatternExample/PaymentContext.java
@@ -0,0 +1,15 @@
+package com.cognizant.strategy;
+
+public class PaymentContext {
+
+    private PaymentStrategy strategy;
+
+    public PaymentContext(PaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void executePayment(double amount) {
+
+        strategy.pay(amount);
+    }
+}
