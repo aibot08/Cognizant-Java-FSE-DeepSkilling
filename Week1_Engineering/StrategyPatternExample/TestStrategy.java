490948adc821b6dfa4201f9840edcc55a8ee1cdc
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-23 16:02:29 +0530

    Add TestStrategy.java

---
+++ Week1_Engineering/StrategyPatternExample/TestStrategy.java
@@ -0,0 +1,19 @@
+package com.cognizant.strategy;
+
+public class TestStrategy {
+
+    public static void main(String[] args) {
+
+        PaymentContext creditCard =
+                new PaymentContext(
+                        new CreditCardPayment());
+
+        creditCard.executePayment(5000);
+
+        PaymentContext paypal =
+                new PaymentContext(
+                        new PayPalPayment());
+
+        paypal.executePayment(2500);
+    }
+}
