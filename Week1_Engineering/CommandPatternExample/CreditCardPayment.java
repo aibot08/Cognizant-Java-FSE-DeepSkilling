738bcd24050af2f0cd2153c6086be626ce130f9d
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-23 16:19:21 +0530

    Add CreditCardPayment.java

---
+++ Week1_Engineering/CommandPatternExample/CreditCardPayment.java
@@ -0,0 +1,10 @@
+package com.cognizant.strategy;
+
+public class CreditCardPayment implements PaymentStrategy {
+    public void pay(double amount) {
+
+        System.out.println(
+                "Paid Rs." + amount +
+                " using Credit Card");
+    }
+}
