4e7608176d867c696c6d1283d1defa3e1e16c290
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-23 16:17:30 +0530

    Add RemoteControl.java

---
+++ Week1_Engineering/CommandPatternExample/RemoteControl.java
@@ -0,0 +1,14 @@
+package com.cognizant.commandpattern;
+
+public class RemoteControl {
+
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
