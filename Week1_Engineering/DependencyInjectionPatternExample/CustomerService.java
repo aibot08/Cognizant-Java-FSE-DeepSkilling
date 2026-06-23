6ae69e98e6b1415835ec6fcbfa4fcbd39d45a4fc
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-06-23 23:02:15 +0530

    Implement CustomerService.java

---
+++ Week1_Engineering/DependencyInjectionPatternExample/CustomerService.java
@@ -0,0 +1,18 @@
+package com.cognizant.di;
+
+public class CustomerService {
+
+    private CustomerRepository repository;
+
+    public CustomerService(
+            CustomerRepository repository) {
+
+        this.repository = repository;
+    }
+
+    public void getCustomer(int id) {
+
+        System.out.println(
+                repository.findCustomerById(id));
+    }
+}
