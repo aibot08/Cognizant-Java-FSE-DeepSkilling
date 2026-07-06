7c7d6098cfabd787d9e3086d0468dea9d05a2ebf
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-07-06 13:09:45 +0530

    Create HasSufficientBalance function for account checks

---
+++ Week1-Module3-PLSQL/Excercise4/Scenario3.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION HasSufficientBalance(
+    p_accountId NUMBER,
+    p_amount NUMBER
+)
+RETURN VARCHAR2
+AS
+    v_balance NUMBER;
+BEGIN
+    SELECT Balance
+    INTO v_balance
+    FROM Accounts
+    WHERE AccountID = p_accountId;
+
+    IF v_balance >= p_amount THEN
+        RETURN 'TRUE';
+    ELSE
+        RETURN 'FALSE';
+    END IF;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 'ACCOUNT NOT FOUND';
+END;
+/
