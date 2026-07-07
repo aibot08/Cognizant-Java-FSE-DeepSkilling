31e680bd75a58893bfd493ff8d01ed743a0527ea
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-07-07 09:44:48 +0530

    Add BookRepository class with saveBook method

---
+++ Week2-HandsOn/Module5-SpringCore and Maven/Excercise3/BookRepository.java
@@ -0,0 +1,10 @@
+	package com.library.repository;
+	
+	import org.springframework.stereotype.Repository;
+
+	public class BookRepository {
+	
+	    public void saveBook() {
+	        System.out.println("Book Saved Successfully");
+	    }
+	}
