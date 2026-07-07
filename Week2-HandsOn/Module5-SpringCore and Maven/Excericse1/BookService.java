9cf6841a20f93ad6e10d40613f0f02cb78f7099c
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-07-07 09:07:24 +0530

    Remove @Service annotation from BookService
    Removed the @Service annotation from BookService class.

---
+++ Week2-HandsOn/Module5-SpringCore and Maven/Excericse1/BookService.java
@@ -0,0 +1,9 @@
+package com.library.service;
+
+import org.springframework.stereotype.Service;
+public class BookService {
+
+    public void display() {
+        System.out.println("Book Service is Working");
+    }
+}
