c02413f76fc093b20ae2ba3553905f4657d642e0
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-07-07 20:21:55 +0530

    Insert stock data into Data.sql
    Add initial stock data for Facebook and Google.

---
+++ Week2-HandsOn/Spring Data JPA with Spring Boot, Hibernate/Excercise2/Data.sql
@@ -0,0 +1,11 @@
+INSERT INTO stock (st_id, st_code, st_date, st_open, st_close, st_volume)
+VALUES (1, 'FB', '2019-09-01', 180.5, 182.3, 100000);
+
+INSERT INTO stock (st_id, st_code, st_date, st_open, st_close, st_volume)
+VALUES (2, 'FB', '2019-09-10', 183.2, 185.1, 120000);
+
+INSERT INTO stock (st_id, st_code, st_date, st_open, st_close, st_volume)
+VALUES (3, 'FB', '2019-10-01', 190.5, 191.2, 150000);
+
+INSERT INTO stock (st_id, st_code, st_date, st_open, st_close, st_volume)
+VALUES (4, 'GOOG', '2019-09-15', 1200.5, 1215.2, 90000);
