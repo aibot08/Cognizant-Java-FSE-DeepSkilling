cc3088ca82c034035a1cea137d68c68a35adf887
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-07-07 10:58:23 +0530

    Add Book entity class with id and title fields

---
+++ Week2-HandsOn/Module5-SpringCore and Maven/Excercise9-SpringBoot/Book.java
@@ -0,0 +1,28 @@
+package com.library.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    private int id;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
