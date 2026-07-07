c6195522c0ca0843cef4441e5f26443451781cd8
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-07-07 09:44:14 +0530

    Implement BookService for managing books

---
+++ Week2-HandsOn/Module5-SpringCore and Maven/Excercise3/BookService.java
@@ -0,0 +1,20 @@
+package com.library.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository repository;
+
+
+    public void setRepository(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addBook() {
+        repository.saveBook();
+    }
+}
