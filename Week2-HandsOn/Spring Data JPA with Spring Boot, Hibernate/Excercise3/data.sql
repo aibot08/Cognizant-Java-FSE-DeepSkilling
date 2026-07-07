fe3075305c9b40c3ee29d3c69dc4bd3d743a1f62
ROHITH INTURI <163744285+aibot08@users.noreply.github.com>
2026-07-07 20:46:48 +0530

    Add initial data for questions and options

---
+++ Week2-HandsOn/Spring Data JPA with Spring Boot, Hibernate/Excercise3/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO question(qt_id,qt_text)
+VALUES
+(1,'Capital of India?');
+
+INSERT INTO options(op_id,op_text,qt_id)
+VALUES
+(1,'Delhi',1),
+(2,'Mumbai',1),
+(3,'Chennai',1),
+(4,'Hyderabad',1);
